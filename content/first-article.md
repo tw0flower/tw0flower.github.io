3441323e53697a25200d61b071228c09ac0601d7
tw0flower <tw0flower@ankhmorpork.host>
2023-02-06 15:48:49 +0900

    Add themes

--- content/first-article.md
+++ content/first-article.md
@@ -2,5 +2,4 @@ Title: My First Review
 Date: 2023-02-05 10:20
 Category: Project
 
-Following is a review of my favorite mechanical keyboard.
-
+A first page.
--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'tw0flower.github.io'
+SITEURL = 'https://tw0flower.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
